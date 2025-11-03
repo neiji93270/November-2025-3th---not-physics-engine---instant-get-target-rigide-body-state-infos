982969575a142fa05ca45e2cc50675ecca91ed6e
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-11-03 05:47:21 +0100

    Create Getilldefinedhypothesissescpuai.h

---
+++ Getilldefinedhypothesissescpuai.h
@@ -0,0 +1,6 @@
+//the main algorithm is only aware of those three numbers of line of codes: 15, 5 and 100. 
+//100 line of code could be erronous although. 
+//to keep track of futur line of codes, its better to use 17 hypothesises
+//. abstract object olatonicism dont exist here, then cpu pointers are safe if the cpu is using 100 registers
+//because clock is unsafe. to get safety we need algebraic topology formulas then
+//this is ill defined.
